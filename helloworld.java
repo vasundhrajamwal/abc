63662f338840b4ce8cb2714c9c81c0b1cbd33cd0
Vasundhra <vasundhra.jamwal@gmail.com>
2022-11-24 13:55:43 +0530

    Helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld {
+public static void main (String args[]) {
+while(true) {
+System.out.println("Hello to terminal commands");
+try {
+Thread.sleep(60000);
+} catch (Exception ex) {}
+}
+}
+}
